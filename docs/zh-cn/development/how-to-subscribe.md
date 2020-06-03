74bf93da5204232512730d62c97abe5b93c628a5
Guangxu Cheng <gxcheng@apache.org>
2020-06-03 10:02:02 +0800

    [TUBEMQ-187] [website] Add development page for showing documents used by developers (#10)

---
+++ docs/zh-cn/development/how-to-subscribe.md
@@ -0,0 +1,32 @@
+# 订阅邮件列表
+
+## 1. 前言
+邮件列表是Apache社区用来沟通交流的一种形式，通常来说，Apache社区的许多事情都是由邮件列表来承载，比如：项目的提问与解答、技术讨论、事务决策、版本发布投票等等，
+订阅邮件后，你可以第一时间获取TubeMQ社区的动态，可以与社区保持同步。
+
+#### TubeMQ项目邮件列表
+
+|名称|描述|订阅邮件|退订邮件|邮件归档|
+|:-----|:--------|:------|:-------|:-----|
+| [dev@tubemq.apache.org](mailto:dev@tubemq.apache.org) | 社区活动信息 | [订阅](mailto:dev-subscribe@tubemq.apache.org)   | [退订](mailto:dev-unsubscribe@tubemq.apache.org)   | [归档](http://mail-archives.apache.org/mod_mbox/tubemq-dev)   |
+| [commits@tubemq.apache.org](mailto:commits@tubemq.apache.org) | 代码库更新信息 | [订阅](mailto:commits-subscribe@tubemq.apache.org)   | [退订](mailto:commits-unsubscribe@tubemq.apache.org)   | [归档](http://mail-archives.apache.org/mod_mbox/tubemq-commits)   |
+
+
+
+## 2. 订阅邮件列表
+**以订阅dev@tubemq.apache.org邮件列表为例**
+
+具体步骤如下：
+ 1. 发送一封不包含任何内容或主题的邮件到: `dev-subscribe@tubemq.apache.org`
+ 2. 等待直到收到一封主题为 `confirm subscribe to dev@tubemq.apache.org` 的邮件(如果长时间未能收到，请确认该邮件是否被你的邮箱拦截，确定没有被拦截且长时间会收到回复邮件的话，返回第1步）
+ 3. 直接回复该邮件，不用修改主题和添加邮件内容。
+ 4. 等待直到收到一封主题为 `WELCOME to dev@tubemq.apache.org` 的邮件。
+ 5. 收到(4)的邮件后，就说明订阅邮件成功，​若想发起讨论，直接往`dev@tubemq.apache.org`发送邮件即可，所有订阅了邮件列表的人都会收到邮件。
+
+## 3. 退订邮件列表
+退订邮件列表的步骤与订阅邮件列表类似，具体步骤如下：
+1. 发送一封不包含任何内容或主题的邮件到: `dev-unsubscribe@tubemq.apache.org`
+2. 等待直到收到一封主题为 `confirm unsubscribe from dev@tubemq.apache.org` 的邮件
+3. 直接回复该邮件，不用修改主题和添加邮件内容
+4. 等待直到收到一封主题为 `GOODBYE from dev@tubemq.apache.org`的邮件
+5. 退订成功
---
+++ site_config/development.js
@@ -0,0 +1,86 @@
+export default {
+  'en-us': {
+    sidemenu: [
+      {
+        title: 'Contribution Guide',
+        children: [
+          {
+            title: 'How to contribute',
+            link: '/en-us/docs/development/how-to-contribute.html',
+          },
+          {
+            title: 'How to commit',
+            link: '/en-us/docs/development/how-to-commit.html',
+          },
+          {
+            title: 'How to subscribe mail list',
+            link: '/en-us/docs/development/how-to-subscribe.html',
+          },
+          {
+            title: 'How to use jira',
+            link: '/en-us/docs/development/how-to-use-jira.html',
+          },
+          {
+            title: 'How to become a committer',
+            link: '/en-us/docs/development/how-to-become-a-committer.html',
+          }
+        ],
+      }, {
+        title: 'Release Guide',
+        children: [
+          {
+            title: 'How to release',
+            link: '/en-us/docs/development/how-to-release.html',
+          },
+          {
+            title: 'How to verify',
+            link: '/en-us/docs/development/how-to-verify.html',
+          }
+        ],
+      }
+    ],
+    barText: 'Development'
+  },
+  'zh-cn': {
+    sidemenu: [
+      {
+        title: '贡献指南',
+        children: [
+          {
+            title: '如何参与贡献',
+            link: '/zh-cn/docs/development/how-to-contribute.html',
+          },
+          {
+            title: '如何提交代码',
+            link: '/zh-cn/docs/development/how-to-commit.html',
+          },
+          {
+            title: '订阅邮件列表',
+            link: '/zh-cn/docs/development/how-to-subscribe.html',
+          },
+          {
+            title: '如何使用JIRA',
+            link: '/zh-cn/docs/development/how-to-use-jira.html',
+          },
+          {
+            title: '如何成为Committer',
+            link: '/zh-cn/docs/development/how-to-become-a-committer.html',
+          }
+        ],
+      }, {
+        title: '版本发布',
+        children: [
+          {
+            title: '如何发布版本',
+            link: '/zh-cn/docs/development/how-to-release.html',
+          },
+          {
+            title: '如何验证版本',
+            link: '/zh-cn/docs/development/how-to-verify.html',
+          }
+        ],
+      }
+    ],
+    barText: '开发'
+  }
+};
