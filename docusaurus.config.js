44f564b0ddf446972fd1af6b00ab34610373b195
wangblue9 <yydsk9@gmail.com>
2021-11-09 18:29:55 +0800

    Use black for code block background style

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -183,7 +183,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         </div>`,
       },
       prism: {
-        theme: lightCodeTheme,
+        // theme: lightCodeTheme,
+        theme: require('prism-react-renderer/themes/dracula'),
         darkTheme: darkCodeTheme,
       },
     }),
