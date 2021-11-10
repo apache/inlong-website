7f0ce4d136724a26dc88086f0f5b7b60c074968c
wangblue9 <yydsk9@gmail.com>
2021-11-10 19:07:40 +0800

    [INLONG-1583]add blog tab

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             activeBaseRegex: `/development/`,
           },
           {
-            to: '/blog/apache-inlong-0.11.0',
+            to: '/blog',
             label: 'BLOG',
             position: 'right'
           },
