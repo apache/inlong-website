1d6d727c75486b3739cc457a31dd8a0bbcfbdb11
bluewang <88174078+bluewang@users.noreply.github.com>
2021-12-02 19:50:18 +0800

    [INLONG-1878] Optimize user display page layout style (#212)

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -22,34 +22,34 @@ export default function() {
                     <p>{dataSource.content}</p>
                 </div>
                 <div className="users-middle-div">
-                    <div className="middle-div3">
+                    <div className="middle-div1">
                         <div className="img-div1">
-                            <img src="/img/users/data-logo.png" className="img-style img-color" alt="腾讯大数据"/>
-                        </div>
-                    </div>
-                    <div className="middle-div2">
-                        <div className="img-div1">
-                            <img src="/img/users/game-logo.png" className="img-style img-color" alt="腾讯游戏"/>
-                        </div>
-                    </div>
-                    <div className="middle-div2">
-                        <div className="img-div1">
-                            <img src="/img/users/pay-logo.png" className="img-style img-color" alt="微信支付"/>
-                        </div>
-                    </div>
-                    <div className="middle-div2">
-                        <div className="img-div1">
-                            <img src="/img/users/meetting-logo.png" className="img-style img-color"  alt="腾讯会议"/>
-                        </div>
-                    </div>
-                    <div className="middle-div2">
-                        <div className="img-div1">
-                            <img src="/img/users/money-logo.png" className="img-style img-color"  alt="财付通"/>
+                            <img src="/img/users/data-logo.png" className="img-style" alt="腾讯大数据"/>
                         </div>
                     </div>
                     <div className="middle-div1">
                         <div className="img-div1">
-                            <img src="/img/users/brain-logo.png" className="img-style img-color" alt="平台大脑"/>
+                            <img src="/img/users/game-logo.png" className="img-style" alt="腾讯游戏"/>
+                        </div>
+                    </div>
+                    <div className="middle-div1">
+                        <div className="img-div1">
+                            <img src="/img/users/pay-logo.png" className="img-style" alt="微信支付"/>
+                        </div>
+                    </div>
+                    <div className="middle-div1">
+                        <div className="img-div1 middle-div-margin3">
+                            <img src="/img/users/meetting-logo.png" className="img-style"  alt="腾讯会议"/>
+                        </div>
+                    </div>
+                    <div className="middle-div1">
+                        <div className="img-div1 middle-div-margin2">
+                            <img src="/img/users/money-logo.png" className="img-style"  alt="财付通"/>
+                        </div>
+                    </div>
+                    <div className="middle-div1">
+                        <div className="img-div1 middle-div-margin1">
+                            <img src="/img/users/brain-logo.png" className="img-style" alt="平台大脑"/>
                         </div>
                     </div>
                 </div>
