347c2891fa39bbf45ab168f490a0b32d70e8d609
dockerzhang <dockerzhang@users.noreply.github.com>
2022-01-04 05:08:26 +0000

    deploy: 400e321366a8f8997b47b1a42a37027f741bea20

---
+++ zh-CN/assets/js/814f3328.1b0f16a9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs_website=self.webpackChunkdocs_website||[]).push([[2535],{45641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"0.11.0 \u7248\u672c\u53d1\u5e03","permalink":"/zh-CN/blog/apache-inlong-0.11.0"},{"title":"0.12.0 \u7248\u672c\u53d1\u5e03","permalink":"/zh-CN/blog/apache-inlong-0.12.0"}]}')}}]);
