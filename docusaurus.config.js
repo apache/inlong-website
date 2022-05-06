b1489c041a4cacec7e0763486c82db86c0ce9888
gosonzhang <4675739@qq.com>
2022-05-06 10:49:26 +0800

    [INLONG-376][Doc]Modify Disclaimer and add Privacy link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -159,6 +159,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
                 to: "https://www.apache.org/foundation/sponsorship.html",
               },
               {
+                label: "Privacy",
+                to: "https://www.apache.org/foundation/policies/privacy.html",
+              },
+              {
                 label: "Thanks",
                 to: "https://www.apache.org/foundation/thanks.html",
               },
@@ -225,7 +229,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         },
         copyright: `<div style="text-align: left;">
           <div>
-            <p style="font-family: Avenir-Medium;font-size: 14px;color: #999;line-height: 20px;"> Apache InLong is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the name of Apache TLP sponsor. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF. </p>
+            <p style="font-family: Avenir-Medium;font-size: 14px;color: #999;line-height: 20px;"> Apache InLong is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the Apache Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF. </p>
           </div>
           <div style="border-top: 1px solid #ccc;min-height: 60px;line-height: 20px;text-align: center;font-family: Avenir-Medium;font-size: 14px;color: #999;display: flex;align-items: center;"><span>Copyright © 2019-2022 The Apache Software Foundation. Apache InLong, InLong, and its feather logo are trademarks of The Apache Software Foundation.</span></div>
         </div>`,
