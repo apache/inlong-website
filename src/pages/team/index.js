7500ef3ec004ed4ecc7721bac272f90f4de27314
Lizhen <88174078+bluewang@users.noreply.github.com>
2022-11-17 14:07:22 +0800

    [INLONG-602] Optimize the team page to better display the team (#603)

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -4,7 +4,7 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import config from "../team/config.json";
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import './index.less';
+import styles from './team.module.less';
 
 export default function() {
     const isBrowser = useIsBrowser();
@@ -17,12 +17,10 @@ export default function() {
 
     const { siteConfig } = useDocusaurusContext();
     const team = siteConfig.customFields.team;
-    const contributors = siteConfig.customFields.contributors;
-    const docContributors = siteConfig.customFields.docContributors;
 
     return (
         <Layout>
-            <div className="team-div"><br/>
+            <div className={styles.teamDiv}><br/>
                 <h1>InLong Team</h1>
                 <div>
                     <p>{dataSource.synopsis[0]}</p>
@@ -31,75 +29,58 @@ export default function() {
                 </div>
                 <h2>Members</h2>
                 <p>{dataSource.members}</p>
-                <table>
-                    <thead>
-                        <tr>
-                            <td><b>Apache ID</b></td>
-                            <td><b>GitHub User Name</b></td>
-                            <td><b>Public Name</b></td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            team.members.map((member, i) => (
-                                <tr key={i} index={i}>
-                                    <td>{member.apacheID}</td>
-                                    <td>{member.githubName}</td>
-                                    <td>{member.publicName}</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
+                <div className={styles.card}>
+                    {
+                        team.members.map((member, i) => (
+                            <div>
+                                <a className={styles.memberLink} target="_blank" href={`https://github.com/${member.githubName}`}>
+                                    <img src={member.avatarUrl} className={styles.memberAvatarImg}/>
+                                    <div className={styles.memberNameDiv}>{member.apacheID}</div>
+                                    <div className={styles.usernameDiv}>{member.githubName}</div>
+                                </a>
+                            </div>
+                        ))
+                    }
+                </div>
                 <h2>Committer</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <td><b>Apache ID</b></td>
-                            <td><b>GitHub User Name</b></td>
-                            <td><b>Public Name</b></td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            team.committers.map((committer, i) => (
-                                <tr key={i} index={i}>
-                                    <td>{committer.apacheID}</td>
-                                    <td>{committer.githubName}</td>
-                                    <td>{committer.publicName}</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
+                <div className={styles.card}>
+                    {
+                        team.committers.map((committer, i) => (
+                            <div>
+                                <a className={styles.memberLink} target="_blank" href={committer.githubName === "" ? `` : `https://github.com/${committer.githubName}`}>
+                                    <img className={styles.memberAvatarImg} src={committer.avatarUrl !== "" ? committer.avatarUrl : useBaseUrl('/img/default.png')}/>
+                                    <div className={styles.memberNameDiv}>{committer.apacheID}</div>
+                                    <div className={styles.usernameDiv}>{committer.githubName}</div>
+                                </a>
+                            </div>
+                        ))
+                    }
+                </div>
                 <h2>Contributors</h2>
                 <p>{dataSource.code_contributors}</p>
-                <div className="contributors-div">
-                    <ul id="conID" className="ul-flex">
-                        {
-                            contributors.map((contributor, i) => (
-                                <li className="mb-2" key={i} index={i}>
-                                    <a href={contributor.html_url} target="_blank">
-                                        <img src={contributor.avatar_url} /><span>{contributor.login}</span>
-                                    </a>
-                                </li>
-                            ))
-                        }
-                    </ul>
+                <div className={styles.committerDiv}>
+                    <a href="https://github.com/apache/inlong/graphs/contributors" className={styles.committerLink} target="_blank">
+                        <svg t="1668397861887" className="icon" viewBox="0 0 1024 1024" version="1.1"
+                             xmlns="http://www.w3.org/2000/svg" p-id="2713" width="32" height="32">
+                            <path
+                                d="M960 512c0 97.76-28.704 185.216-85.664 263.264-56.96 78.016-130.496 131.84-220.64 161.856-10.304 1.824-18.368 0.448-22.848-4.032a22.4 22.4 0 0 1-7.2-17.504v-122.88c0-37.632-10.304-65.44-30.464-82.912a409.856 409.856 0 0 0 59.616-10.368 222.752 222.752 0 0 0 54.72-22.816c18.848-10.784 34.528-23.36 47.104-38.592 12.544-15.232 22.848-35.904 30.912-61.44 8.096-25.568 12.128-54.688 12.128-87.904 0-47.072-15.232-86.976-46.208-120.16 14.368-35.456 13.024-74.912-4.48-118.848-10.752-3.616-26.432-1.344-47.072 6.272s-38.56 16.16-53.824 25.568l-21.984 13.888c-36.32-10.304-73.536-15.232-112.096-15.232s-75.776 4.928-112.096 15.232a444.48 444.48 0 0 0-24.672-15.68c-10.336-6.272-26.464-13.888-48.896-22.432-21.952-8.96-39.008-11.232-50.24-8.064-17.024 43.936-18.368 83.424-4.032 118.848-30.496 33.632-46.176 73.536-46.176 120.608 0 33.216 4.032 62.336 12.128 87.456 8.032 25.12 18.368 45.76 30.496 61.44 12.544 15.68 28.224 28.704 47.072 39.04 18.848 10.304 37.216 17.92 54.72 22.816a409.6 409.6 0 0 0 59.648 10.368c-15.712 13.856-25.12 34.048-28.704 60.064a99.744 99.744 0 0 1-26.464 8.512 178.208 178.208 0 0 1-33.184 2.688c-13.024 0-25.568-4.032-38.144-12.544-12.544-8.512-23.296-20.64-32.256-36.32a97.472 97.472 0 0 0-28.256-30.496c-11.232-8.064-21.088-12.576-28.704-13.92l-11.648-1.792c-8.096 0-13.92 0.928-17.056 2.688-3.136 1.792-4.032 4.032-2.688 6.72s3.136 5.408 5.376 8.096 4.928 4.928 7.616 7.168l4.032 2.688c8.544 4.032 17.056 11.232 25.568 21.984 8.544 10.752 14.368 20.64 18.4 29.6l5.824 13.44c4.928 14.816 13.44 26.912 25.568 35.872 12.096 8.992 25.088 14.816 39.008 17.504 13.888 2.688 27.36 4.032 40.352 4.032s23.776-0.448 32.288-2.24l13.472-2.24c0 14.784 0 32.288 0.416 52.032 0 19.744 0.48 30.496 0.48 31.392a22.624 22.624 0 0 1-7.648 17.472c-4.928 4.48-12.992 5.824-23.296 4.032-90.144-30.048-163.68-83.84-220.64-161.888C92.256 697.216 64 609.312 64 512c0-81.152 20.192-156.064 60.096-224.672s94.176-122.88 163.232-163.232C355.936 84.192 430.816 64 512 64s156.064 20.192 224.672 60.096 122.88 94.176 163.232 163.232C939.808 355.488 960 430.848 960 512"
+                                fill="#000000" p-id="2714"></path>
+                        </svg>
+                        apache/inlong
+                    </a>
+                    <a href="https://github.com/apache/inlong-website/graphs/contributors" className={styles.committerLink} target="_blank">
+                        <svg t="1668397861887" className="icon" viewBox="0 0 1024 1024" version="1.1"
+                             xmlns="http://www.w3.org/2000/svg" p-id="2713" width="32" height="32">
+                            <path
+                                d="M960 512c0 97.76-28.704 185.216-85.664 263.264-56.96 78.016-130.496 131.84-220.64 161.856-10.304 1.824-18.368 0.448-22.848-4.032a22.4 22.4 0 0 1-7.2-17.504v-122.88c0-37.632-10.304-65.44-30.464-82.912a409.856 409.856 0 0 0 59.616-10.368 222.752 222.752 0 0 0 54.72-22.816c18.848-10.784 34.528-23.36 47.104-38.592 12.544-15.232 22.848-35.904 30.912-61.44 8.096-25.568 12.128-54.688 12.128-87.904 0-47.072-15.232-86.976-46.208-120.16 14.368-35.456 13.024-74.912-4.48-118.848-10.752-3.616-26.432-1.344-47.072 6.272s-38.56 16.16-53.824 25.568l-21.984 13.888c-36.32-10.304-73.536-15.232-112.096-15.232s-75.776 4.928-112.096 15.232a444.48 444.48 0 0 0-24.672-15.68c-10.336-6.272-26.464-13.888-48.896-22.432-21.952-8.96-39.008-11.232-50.24-8.064-17.024 43.936-18.368 83.424-4.032 118.848-30.496 33.632-46.176 73.536-46.176 120.608 0 33.216 4.032 62.336 12.128 87.456 8.032 25.12 18.368 45.76 30.496 61.44 12.544 15.68 28.224 28.704 47.072 39.04 18.848 10.304 37.216 17.92 54.72 22.816a409.6 409.6 0 0 0 59.648 10.368c-15.712 13.856-25.12 34.048-28.704 60.064a99.744 99.744 0 0 1-26.464 8.512 178.208 178.208 0 0 1-33.184 2.688c-13.024 0-25.568-4.032-38.144-12.544-12.544-8.512-23.296-20.64-32.256-36.32a97.472 97.472 0 0 0-28.256-30.496c-11.232-8.064-21.088-12.576-28.704-13.92l-11.648-1.792c-8.096 0-13.92 0.928-17.056 2.688-3.136 1.792-4.032 4.032-2.688 6.72s3.136 5.408 5.376 8.096 4.928 4.928 7.616 7.168l4.032 2.688c8.544 4.032 17.056 11.232 25.568 21.984 8.544 10.752 14.368 20.64 18.4 29.6l5.824 13.44c4.928 14.816 13.44 26.912 25.568 35.872 12.096 8.992 25.088 14.816 39.008 17.504 13.888 2.688 27.36 4.032 40.352 4.032s23.776-0.448 32.288-2.24l13.472-2.24c0 14.784 0 32.288 0.416 52.032 0 19.744 0.48 30.496 0.48 31.392a22.624 22.624 0 0 1-7.648 17.472c-4.928 4.48-12.992 5.824-23.296 4.032-90.144-30.048-163.68-83.84-220.64-161.888C92.256 697.216 64 609.312 64 512c0-81.152 20.192-156.064 60.096-224.672s94.176-122.88 163.232-163.232C355.936 84.192 430.816 64 512 64s156.064 20.192 224.672 60.096 122.88 94.176 163.232 163.232C939.808 355.488 960 430.848 960 512"
+                                fill="#000000" p-id="2714"></path>
+                        </svg>
+                        apache/inlong-website
+                    </a>
                 </div>
-                <p>{dataSource.doc_contributors}</p>
-                <div className="contributors-div">
-                    <ul id="docID" className="ul-flex">
-                        {
-                            docContributors.map((docContributor, i) => (
-                                <li className="mb-2" key={i} index={i}>
-                                    <a href={docContributor.html_url} target="_blank">
-                                        <img src={docContributor.avatar_url} /><span>{docContributor.login}</span>
-                                    </a>
-                                </li>
-                            ))
-                        }
-                    </ul>
+                <h2>Contributor Over Time</h2>
+                <div className={styles.divImg}>
+                    <img src="https://contributor-graph-api.apiseven.com/contributors-svg?repo=apache/inlong&merge=true" alt="apache inlong"/>
                 </div>
             </div>
         </Layout>
