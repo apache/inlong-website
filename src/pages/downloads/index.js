552195352a8b12fd4d11e99541acd44451557f29
Lizhen <88174078+bluewang@users.noreply.github.com>
2023-09-14 17:08:50 +0800

    [INLONG-841] Download page display error on a small screen (#851)

--- src/pages/downloads/index.js
+++ src/pages/downloads/index.js
@@ -17,85 +17,92 @@ export default function() {
                 <p>
                     <Translate id="page.download.describe">You can download the latest version here.</Translate>
                 </p>
-                <div className={styles.cardDiv}>
-                    <div className={styles.card}>
-                        <div className={styles.leftSide}>
-                            <div className={styles.title}>InLong Source Code</div>
-                            <div className={styles.description}>Here you can download files to InLong source code</div>
-                        </div>
-                        <div className={styles.rightSide}>
-                            <div className={styles.versionInfo}>
-                                Latest Version ·
-                                <span>{version}</span><br/>
-                                Release Date ·
-                                <span>{date}</span>
-                            </div>
-                            <div className={styles.buttonRow}>
-                                <div className={styles.buttonCard}>
-                                    <div className={styles.styledDropdown}>
-                                        <a className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-src.tar.gz`}>SRC</a>
-                                        <a target="_blank" className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-src.tar.gz.asc`}>ASC</a>
-                                        <a target="_blank" className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-src.tar.gz.sha512`}>SHA512</a>
+                <section className={styles.cardSection}>
+                    <ul className={styles.cardUl}>
+                        <li>
+                            <div className={styles.card}>
+                                <div className={styles.leftSide}>
+                                    <div className={styles.title}>InLong Source Code</div>
+                                    <div className={styles.description}>Here you can download files to InLong source code</div>
+                                </div>
+                                <div className={styles.rightSide}>
+                                    <div className={styles.versionInfo}>
+                                        Latest Version ·
+                                        <span>{version}</span><br/>
+                                        Release Date ·
+                                        <span>{date}</span>
+                                    </div>
+                                    <div className={styles.buttonRow}>
+                                        <div className={styles.buttonCard}>
+                                            <div className={styles.styledDropdown}>
+                                                <a className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-src.tar.gz`}>SRC</a>
+                                                <a target="_blank" className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-src.tar.gz.asc`}>ASC</a>
+                                                <a target="_blank" className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-src.tar.gz.sha512`}>SHA512</a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-
-                    <div className={styles.card} style={{ marginLeft: 30}}>
-                        <div className={styles.leftSide}>
-                            <div className={styles.title}>InLong Binary file</div>
-                            <div className={styles.description}>The InLong binary can be downloaded here</div>
-                        </div>
-                        <div className={styles.rightSide}>
-                            <div className={styles.versionInfo}>
-                                Latest Version ·
-                                <span>{version}</span><br/>
-                                Release Date ·
-                                <span>{date}</span>
-                            </div>
-                            <div className={styles.buttonRow}>
-                                <div className={styles.buttonCard}>
-                                    <div className={styles.styledDropdown}>
-                                        <a className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-bin.tar.gz`}>BIN</a>
-                                        <a target="_blank" className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-bin.tar.gz.asc`}>ASC</a>
-                                        <a target="_blank" className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-bin.tar.gz.sha512`}>SHA512</a>
+                        </li>
+                        <li>
+                            <div className={styles.card}>
+                                <div className={styles.leftSide}>
+                                    <div className={styles.title}>InLong Binary file</div>
+                                    <div className={styles.description}>The InLong binary can be downloaded here</div>
+                                </div>
+                                <div className={styles.rightSide}>
+                                    <div className={styles.versionInfo}>
+                                        Latest Version ·
+                                        <span>{version}</span><br/>
+                                        Release Date ·
+                                        <span>{date}</span>
+                                    </div>
+                                    <div className={styles.buttonRow}>
+                                        <div className={styles.buttonCard}>
+                                            <div className={styles.styledDropdown}>
+                                                <a target="_blank" className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-bin.tar.gz`}>BIN</a>
+                                                <a target="_blank" className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-bin.tar.gz.asc`}>ASC</a>
+                                                <a target="_blank" className={styles.dropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-bin.tar.gz.sha512`}>SHA512</a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className={styles.card} style={{ marginLeft: 30}}>
-                        <div className={styles.leftSide}>
-                            <div className={styles.title}>Connector Binary file</div>
-                            <div className={styles.description}>Here you can download files to InLong connector binaries</div>
-                        </div>
-                        <div className={styles.rightSide}>
-                            <div className={styles.versionInfo}>
-                                Latest Version ·
-                                <span>{version}</span><br/>
-                                Release Date ·
-                                <span>{date}</span>
-                            </div>
-                            <div className={styles.buttonRow}>
-                                <div className={styles.buttonCard}>
-                                    <div className={styles.connectorDropdownOne}>
-                                        <span className={styles.connectorSpan}>v1.13:</span>
-                                        <a className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.13.tar.gz`}>BIN</a>
-                                        <a target="_blank" className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.13.tar.gz.asc`}>ASC</a>
-                                        <a target="_blank" className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.13.tar.gz.sha512`}>SHA512</a>
+                        </li>
+                        <li>
+                            <div className={styles.card}>
+                                <div className={styles.leftSide}>
+                                    <div className={styles.title}>Connector Binary file</div>
+                                    <div className={styles.description}>Here you can download files to InLong connector binaries</div>
+                                </div>
+                                <div className={styles.rightSide}>
+                                    <div className={styles.versionInfo}>
+                                        Latest Version ·
+                                        <span>{version}</span><br/>
+                                        Release Date ·
+                                        <span>{date}</span>
                                     </div>
-                                    <div className={styles.connectorDropdownTwo}>
-                                        <span className={styles.connectorSpan}>v1.15:</span>
-                                        <a className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.15.tar.gz`}>BIN</a>
-                                        <a target="_blank" className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.15.tar.gz.asc`}>ASC</a>
-                                        <a target="_blank" className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.15.tar.gz.sha512`}>SHA512</a>
+                                    <div className={styles.buttonRow}>
+                                        <div className={styles.buttonCard}>
+                                            <div className={styles.connectorDropdownOne}>
+                                                <span className={styles.connectorSpan}>v1.13:</span>
+                                                <a className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.13.tar.gz`}>BIN</a>
+                                                <a target="_blank" className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.13.tar.gz.asc`}>ASC</a>
+                                                <a target="_blank" className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.13.tar.gz.sha512`}>SHA512</a>
+                                            </div>
+                                            <div className={styles.connectorDropdownTwo}>
+                                                <span className={styles.connectorSpan}>v1.15:</span>
+                                                <a className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.15.tar.gz`}>BIN</a>
+                                                <a target="_blank" className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.15.tar.gz.asc`}>ASC</a>
+                                                <a target="_blank" className={styles.connectorDropdownItem} href={`https://downloads.apache.org/inlong/${version}/apache-inlong-${version}-sort-connectors-flink-v1.15.tar.gz.sha512`}>SHA512</a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
+                        </li>
+                    </ul>
+                </section>
                 <h2>
                     <Translate id="page.download.version">Older releases</Translate>
                 </h2>
